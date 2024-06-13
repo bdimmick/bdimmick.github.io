f0444dd867092eb0dd729a697c0ef8e139d7f4c3
bdimmick <me@billdimmick.com>
2024-06-13 12:23:34 -0700

    changes to formatting and style to be more like proper javascript

--- index.js
+++ index.js
@@ -1,105 +1,110 @@
-execute_on = true
+hiddenFiles = ["index.js"]
 
-deny_cat = ["index.js"]
-
-pad_to = function(val, len) {
-	while (val.length < len) {
-		val = val + " "
-	}
-	return val
+ function addPadding (val, len) {
+    while (val.length < len) {
+        val = val + " "
+    }
+    return val
 }
 
-default_value = function(v, def) {
-	return (typeof v !== 'undefined' ?  v : def)
+valueOrDefault = function (v, def) {
+    return (typeof v !== 'undefined' ? v : def)
 }
 
-display_output = function(output, append) {
-	append = default_value(append, false) 
-	if (append) {
-		output = $('div#output').text() + output +"\n"
-	}
-	$('div#output').text(output)
+displayOutput = function (output, append) {
+    append = valueOrDefault(append, false)
+    if (append) {
+        output = $('div#output').text() + output + "\n"
+    }
+    $('div#output').text(output)
 }
 
-function cat_fail(f) {
-	display_output("cat: " + f + ": No such file or directory", true)
+function failCatCommand(f) {
+    displayOutput("cat: " + f + ": No such file or directory", true)
 }
 
 commands = {
-	'help': [
-		"Displays this message.",
-		function(params) {
-			keys = []
-			maxlen = 0
-			$.each(commands, function(k, v) { 
-				keys.push(k) 
-				if (k.length > maxlen) {
-					maxlen = k.length
-				}
-			})
-			keys.sort()
-			keys.forEach(function(k) {
-				display_output(pad_to(k, maxlen) + " - " + commands[k][0], true)
-			})
-		}
-	],
-	'cat': [
-		"Prints a file.",
-		function(params) { 
-			params.forEach(function(f) {
-				f = f.toLowerCase()
-				if (deny_cat.includes(f)) {
-					cat_fail(f)
-				} else {
-					$.get('/' + f, function(data) { display_output(data, true) }).fail( function() {  cat_fail(f) })
-				}
-			})
-		}
-	],
-	'curl': [
-		"Loads a URL in a new window.",
-		function(params) { window.open(params[0]) }
-	],
-	'clear': [
-		'Clears the output.',
-		function(params) { display_output("") }
-	]
+    'help': [
+        "Displays this message.",
+        function (params) {
+            keys = []
+            maxlen = 0
+            $.each(commands, function (k, v) {
+                keys.push(k)
+                if (k.length > maxlen) {
+                    maxlen = k.length
+                }
+            })
+            keys.sort()
+            keys.forEach(function (k) {
+                displayOutput(addPadding(k, maxlen) + " - " + commands[k][0], true)
+            })
+        }
+    ],
+    'cat': [
+        "Prints a file.",
+        function (params) {
+            params.forEach(function (f) {
+                f = f.toLowerCase()
+                if (hiddenFiles.includes(f)) {
+                    failCatCommand(f)
+                } else {
+                    $.get('/' + f, function (data) {
+                        displayOutput(data, true)
+                    }).fail(function () {
+                        failCatCommand(f)
+                    })
+                }
+            })
+        }
+    ],
+    'curl': [
+        "Loads a URL in a new window.",
+        function (params) {
+            window.open(params[0])
+        }
+    ],
+    'clear': [
+        'Clears the output.',
+        function (params) {
+            displayOutput("")
+        }
+    ]
 }
 
-execute = function(cmd) {
-	if (execute_on) {
-		c = cmd[0]
-		entry = commands[c]
-		display_output("")
-		if (typeof entry != 'undefined') {
-			cmd.shift()
-			entry[1](cmd)
-		} else {
-			display_output("'" + c + "' - command not found.")
-		}
-	}
+function executeCommand (input) {
+    command = input.trim().split(' ')
+    target = command[0]
+    entry = commands[target]
+    displayOutput("")
+    if (typeof entry != 'undefined') {
+        command.shift()
+        entry[1](command)
+    } else {
+        displayOutput("'" + target + "' - command not found.")
+    }
 }
 
-$(function() {
-	$('body').keypress(
-		function(event) {
-			current = $('span#input').text().toString()
-			if (event.keyCode == 13) { // handle return
-				current.split(';').forEach(
-					function (command) {
-						execute(command.trim().split(' '))
-					}
-				)
+$(function () {
+    $('body').keypress(
+        function (event) {
+            current = $('span#input').text().toString()
+            if (event.keyCode == 13) { // handle return
+                current.split(';').forEach(
+                    function (command) {
+                        executeCommand(command.trim().split(' '))
+                    }
+                )
 
-				$('span#input').text('')
-			} else if (event.keyCode == 8) { // handle delete
-				if (current.length > 0) {
-					$('span#input').text(current.slice(0, current.length - 1))
-				}
-			} else {
-				$('span#input').text(current + String.fromCharCode(event.charCode))
-			}
-			event.preventDefault()
-			return false
-		})
+                $('span#input').text('')
+            } else if (event.keyCode == 8) { // handle delete
+                if (current.length > 0) {
+                    $('span#input').text(current.slice(0, current.length - 1))
+                }
+            } else {
+                $('span#input').text(current + String.fromCharCode(event.charCode))
+            }
+            event.preventDefault()
+            return false
+        })
 })
